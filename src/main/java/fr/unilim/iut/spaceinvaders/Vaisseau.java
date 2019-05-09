e833b1f714f1ab443d85c3c7d3fb5d9be724ed9d
Mathieu Zani <mathieu.zani@etu.unilim.fr>
2019-05-09 09:12:05 +0200

    ajout de vitesse au vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -4,19 +4,17 @@ public class Vaisseau {
 
     private Position origine;
     private Dimension dimension;
-
-    public Vaisseau(int longueur, int hauteur) {
-	    this(longueur, hauteur, 0, 0);
-    }
-
-   public Vaisseau(int longueur, int hauteur, int x, int y) {
-	   this(new Dimension(longueur, hauteur), new Position(x, y));
-    }
+    private int vitesse;
 
     public Vaisseau(Dimension dimension, Position positionOrigine) {
-	    this.dimension = dimension;
-	    this.origine = positionOrigine;
-    }
+		this(dimension, positionOrigine, 1);
+	}
+
+	public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
+		this.dimension = dimension;
+		this.origine = positionOrigine;
+		this.vitesse = vitesse;
+	}
 
     public boolean occupeLaPosition(int x, int y) {
 	    return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
@@ -30,11 +28,11 @@ public class Vaisseau {
 	   return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
     }
 
-    private int ordonneeLaPlusBasse() {
+    public int ordonneeLaPlusBasse() {
 	    return this.origine.ordonnee() - this.dimension.hauteur() + 1;
     }
 
-    private int ordonneeLaPlusHaute() {
+    public int ordonneeLaPlusHaute() {
 	   return this.origine.ordonnee();
    }
 
@@ -47,16 +45,20 @@ public class Vaisseau {
    }
 
    public void seDeplacerVersLaDroite() {
-	   this.origine.changerAbscisse(this.origine.abscisse() + 1);
-   }
-
+		this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
+	}
+   
    public void seDeplacerVersLaGauche() {
-	   this.origine.changerAbscisse(this.origine.abscisse() - 1);
-   }
+		this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
+	}
 
    public void positionner(int x, int y) {
 	   this.origine.changerAbscisse(x);
 	   this.origine.changerOrdonnee(y);
    }
 
+	public int longueur() {
+		
+		return dimension.longueur();
+	}
 }
